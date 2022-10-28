00580792f7ffd4e78bd73b299bf0b945a3ba3062
“jessie <sonimac122699@gmail.com>
2022-10-28 21:35:39 +0800

    Added Bootstrap Framework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <div>Guys Pwedi Nang Magamit ang Bootstrap sa Ating Project Na Install kona</div>
+        <div className='btn btn-outline-primary'>Added Bootstrap</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -15,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
